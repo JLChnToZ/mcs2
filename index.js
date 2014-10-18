b32851b81433ba5c8e25510c6620cc428307bb73
JLChnToZ <jeremylam619@gmail.com>
2014-10-18 14:59:34 +0800

    Add new API

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ var express = require("express");
 var mustacheExpress = require("mustache-express");
 var socketio = require("socket.io");
 var minify = require("express-minify");
+var params = require("express-params");
 var compression = require("compression");
+var _ = require("underscore");
 
 var cron = require("./lib/cron");
 var singlerequest = require("./lib/singlerequest");
@@ -16,6 +18,8 @@ ssd(function() {
   var httpserv = http.Server(app);
   var io = socketio(httpserv);
   var singlereq;
+  var iplist = {};
+  var config = {};
   
   var args = argv.option([
     {
@@ -31,14 +35,40 @@ ssd(function() {
     }
   ]).run();
   
+  params.extend(app);
   app.engine("mustache", mustacheExpress());
   app.set("view engine", "mustache");
   app.set("views", __dirname + "/static/templates");
   
   app.get("/", function(req, res) {
-    res.render("index", { status: cron.serverstatus() });
+    var cfg = _.clone(config);
+    cfg.singleUseLimit /= 1000;
+    cfg.fetchServerListPeriod /= 60 * 1000;
+    cfg.pingActiveServerPeriod /= 60 * 1000;
+    cfg.pingInactiveServerPeriod /= 60 * 1000;
+    res.render("index", {
+      status: cron.serverstatus(),
+      config: cfg
+    });
   });
   
+  app.param("ipaddr", /^((?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])|(?:(?:\w|\w[\w\-]*\w)\.)*(?:\w|\w[\w\-]*\w))(?::(0*(?:[1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])))?(?:\.json)?$/i);
+  app.get("/api/:ipaddr", function(req, res) {
+    var clientIP = req.get("x-forwarded-for") || req.connection.remoteAddress;
+    if(!(clientIP in iplist)) iplist[clientIP] = {};
+    singlereq.exposeAPI(iplist[clientIP])({
+      host: req.params.ipaddr[1],
+      port: req.params.ipaddr.length > 2 ? parseInt(req.params.ipaddr[2]) : 25565,
+      addResult: ("addresult" in req.query) ? req.query.addresult == "true" : true,
+      url: req.query.ref || req.get("referer") || "",
+    }, function(err, data) {
+      res.type("application/json");
+      if(err && err.message == "Single use limit exceeds")
+        res.json({ error: err.message });
+      else
+        res.json(data);
+    });
+  });
   
   app.use(function(req, res, next) {
     if (/\.min\.(css|js)$/.test(req.url))
@@ -59,19 +89,28 @@ ssd(function() {
         if(status[i].lastUpdate >= data.timeStamp)
           socket.emit("status_update", status[i]);
     });
-    if(singlereq)
-      singlereq.registerSocket(io, socket);
+    if(singlereq) {
+      var _api = singlereq.exposeAPI();
+      socket.on("request", function(req) {
+        return _api(req, function(err, res) {
+          if(err && err.message == "Single use limit exceeds")
+            socket.emit("single_use_limit_exceeds", {});
+          else
+            socket.emit("single_use_status_update", res || req);
+        });
+      });
+    }
   });
   
   jsonfile.readFile("./config.json", function(err, cfg) {
-    if(!cfg) cfg = {};
-    var runPort = args.options.port || cfg.port || 3838;
-    var runIP = args.options.ip || cfg.ip || "0.0.0.0";
+    if(cfg) config = cfg;
+    var runPort = args.options.port || config.port || 3838;
+    var runIP = args.options.ip || config.ip || "0.0.0.0";
     httpserv.listen(runPort, runIP, function() {
       console.log("Server listening on " + runIP + ":" + runPort);
     });
-    cron.run(io, cfg);
-    singlereq = singlerequest(cron, cfg);
+    cron.run(io, config);
+    singlereq = singlerequest(io, cron, config);
   });
 
 });
